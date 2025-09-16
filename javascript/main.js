73a90a33fcce6d40f9e6480ac7fc9cd98fb62b7f
daors12345678910 <daors.sagonjeva@gmail.com>
2025-09-16 20:43:22 +0200

    moduli1

---
+++ javascript/main.js
@@ -0,0 +1,40 @@
+console.log("nigga")
+
+var kats = 15;
+
+var a=50+50;
+
+console.log(a);
+
+var b=70;
+
+var c=80;
+
+var d= b+c;
+
+console.log(d);
+
+
+var   e =40;
+
+var f = 10;
+
+console.log(e +f);
+
+
+var p=20;
+
+var q=p**2;
+
+console.log(q);
+
+var x =15 ;
+
+x+=5;
+
+console.log(x);
+
+var s=10;
+
+
+
