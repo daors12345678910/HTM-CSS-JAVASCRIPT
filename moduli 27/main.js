4c8c307e7c771302da5af1ba586e97813cd91787
daors12345678910 <daors.sagonjeva@gmail.com>
2025-09-25 20:43:45 +0200

    m 27

---
+++ moduli 27/main.js
@@ -0,0 +1,51 @@
+function numvers(a,b){
+
+
+   return a+b
+     
+
+
+
+
+
+}
+
+
+console.log(numvers(1,2))
+
+
+function numvers2(){
+
+    alert("hi")
+}
+
+function tocel(f){
+
+
+
+    return f*5/9;
+}
+
+console.log("54 f in c is "+tocel(54)+"cel")
+
+
+numvers2()
+
+
+function local() {
+
+
+    
+    
+    var localVar = "heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
+
+    alert(localVar);
+
+
+}
+
+local();
+
+
+
+
