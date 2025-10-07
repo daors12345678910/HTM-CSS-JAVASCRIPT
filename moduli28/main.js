7973b06fa0cc15bd3cd41a47e1b6e8328aceb523
daors12345678910 <daors.sagonjeva@gmail.com>
2025-10-07 20:47:28 +0200

    mod28

---
+++ moduli28/main.js
@@ -0,0 +1,62 @@
+var button1 = document.querySelector("#button1");
+
+
+button1.onclick = function(){
+    alert('helloooo');
+}
+
+
+var button2=document.querySelector("#button2");
+
+
+var button3=document.querySelector("#button3");
+
+
+var button4=document.querySelector("#button4");
+
+
+var button5=document.querySelector("#button5");
+
+
+button2.addEventListener('click', function(){
+    alert('button 222')
+});
+
+
+button3.onmouseover = function(){
+    alert("Mouse over");
+}
+
+
+button4.onmouseleave=function(){
+    alert("On Mouse Leave")
+}
+
+
+button5.onmousewheel=function(){
+    alert("On Mouse Leave")
+}
+
+
+var btn1=document.getElementById("btn1")
+var btn2=document.getElementById("btn2")
+var text=document.getElementById("txt1")
+var tx2=document.querySelector('#txt2')
+
+
+btn1.onclick=function(){
+    text.style.color="red";
+    text.style.backgroundColor="blue";
+    text.style.textAlign='center';
+    tx2.setAttribute("class", "txt2")
+
+
+
+
+}
+
+btn2.onclick=function(){
+
+    text.setAttribute('class', 'tx1')
+  
+}
