c52a9d4ecc340ac757ba5d45be385ca286423c4a
daors12345678910 <daors.sagonjeva@gmail.com>
2025-10-21 20:44:06 +0200

    m31

---
+++ javascript/m31/main.js
@@ -0,0 +1,87 @@
+// Do while loop
+var i = 0;
+
+
+do{
+    i = i + 1;
+    console.log(i);
+}while(i < 5);
+
+
+console.log("------------------------------")
+
+
+ // While loop
+
+
+ var j = 0;
+ while (j < 5){
+    j = j + 1;
+    console.log(j);
+ }
+
+
+ console.log("------------------------------")
+
+
+ let n=0;
+ let x=0;
+ while(n<3 ){
+
+    n++
+    x = x+n;
+
+    console.log(n)
+    console.log(x)
+ }
+
+  console.log("------------------------------")
+
+  for(i = 0; i<5; i++){
+
+    console.log("i value is = "+i)
+  }
+
+
+   console.log("------------------------------")
+
+
+   var person={firstname ="a", secondname="b ",type="alien" }
+
+   var text=" ";
+
+   var z;
+
+ for(z in person){
+
+    text +=person[z]
+ }
+
+
+ let names=['un','ti','ai','ajo']
+
+ let het
+
+ for(het of names){
+
+ console.log(het)
+
+
+ }
+
+
+  let txt=['untiaiajo']
+
+ let he
+
+ for(he of names){
+
+ console.log(he)
+
+
+ }
+
+
+
+
+
